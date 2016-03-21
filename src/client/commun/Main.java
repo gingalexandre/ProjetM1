3781985ef7ff5be404e6a8253e62ee1331c6b7b2
MarintheJerome <jerome.marinthe@gmail.com>
2016-03-21 13:26:04 +0100

    [interface] A-R - Vues avec Fxml

--- src/client/commun/Main.java
+++ src/client/commun/Main.java
@@ -1,31 +1,10 @@
 package client.commun;
-import java.util.ArrayList;
 
-import client.view.VuePlateau;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Polygon;
-import javafx.stage.Stage;
+import client.view.VuePrincipale;
 
-public class Main extends Application {
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		// TODO Auto-generated method stub
-	    VuePlateau p = new VuePlateau();
-	    Scene scene = p.getScene();
-	    stage.setTitle("Plateau de jeu");
-	    stage.setScene(scene);
-	    stage.show();
-	}
+public class Main {
 	
 	public static void main(String[] args){
-		launch(args);
+		VuePrincipale.launch(VuePrincipale.class);
 	}
-	
 }

612f4572a942764fd89a275733a90d1381209863
Alexandre GINGEMBRE <Alexandre@MacBook-Pro-de-Alexandre.local>
2016-03-24 11:53:02 +0100

    <Master> A bandeau utilisateur principal

--- src/client/controller/application/JoueurActuelApplicationController.java
+++ src/client/controller/application/JoueurActuelApplicationController.java
@@ -5,15 +5,16 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 public class JoueurActuelApplicationController implements Initializable {
 
 	@FXML
 	private ImageView imageBle, imageArgile, imageCaillou, imageLaine, imageBois;
-
-	private TextField nbBle, nbArgile, nbCaillou, nbLaine, nbBois;
+	
+	@FXML
+	private Label nbBle, nbArgile, nbCaillou, nbLaine, nbBois;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -23,7 +24,41 @@ public class JoueurActuelApplicationController implements Initializable {
 		nbLaine.setText("0");
 		nbBois.setText("0");
 		
+	}
 
+	/**
+	 * Méthode permettant de modifier la valeur de la ressource passé en paramètre
+	 * @param nomRessource : String nom de la ressource à modifier
+	 * @param nombre : int nombre à modifier
+	 */
+	public void modifierRessource(String nomRessource, int nombre) {
+
+		Label ressource = distribuerLabelRessource(nomRessource);
+		int nbInitial = Integer.parseInt(ressource.getText());
+		ressource.setText(Integer.toString(nbInitial + nombre));
+		
+	}
+
+	/**
+	 * Méthode renvoyant le Label correspondant au nom de la ressource
+	 * @param nomRessource : String : nom de la ressource
+	 * @return Label : label correspondant
+	 */
+	public Label distribuerLabelRessource(String nomRessource) {
+		switch (nomRessource) {
+		case "Ble":
+			return nbBle;
+		case "Argile":
+			return nbArgile;
+		case "Caillou":
+			return nbCaillou;
+		case "Laine":
+			return nbLaine;
+		case "Bois":
+			return nbBois;
+		default:
+			return null;
+		}
 	}
 
 }

9070d7204b2d601d2cc2a9c2b9a749187628f8b9
Alexandre GINGEMBRE <Alexandre@MacBook-Pro-de-Alexandre.local>
2016-03-26 12:40:56 +0100

    [Master] A de la classe JDBC et de sqlite pour le serveur

---
+++ src/serveur/bdd/Base.java
@@ -0,0 +1,31 @@
+package serveur.bdd;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Base {
+	/**
+	 * Fonction permettant la connexion à la Base de Données
+	 * 
+	 * @return Connection : Objet de Connexion à la Base
+	 * @throws InterruptedException 
+	 */
+	public static Connection connexion() throws InterruptedException {
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:database.db");
+			c.setAutoCommit(false);
+		}catch (SQLException sql){
+			System.out.println("Base de données indisponible, le serveur va tenter de se reconnecter d'ici 10 secondes. Merci de patientez");
+			Thread.sleep(10000);
+			return Base.connexion();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		return c;
+	}
+}

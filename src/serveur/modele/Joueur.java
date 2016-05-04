c8c677fb18688fbd327ee432620c67d040820cc9
MarintheJerome <jerome.marinthe@gmail.com>
2016-05-04 20:11:08 +0200

    [master] Suppression commentaires inutiles

--- src/serveur/modele/Joueur.java
+++ src/serveur/modele/Joueur.java
@@ -482,8 +482,6 @@ public class Joueur extends UnicastRemoteObject implements JoueurInterface, Seri
 	 * @throws RemoteException
 	 */
 	public void ajoutRessource(int typeRessource, int value) throws RemoteException {
-		System.out.println(this.stockRessource.keySet());
-		System.out.println(this.stockRessource.keySet().size());
 		if(this.stockRessource.keySet() != null){
 			this.stockRessource.put(typeRessource, this.stockRessource.get(typeRessource) + value);
 		}
